6542da6f19067502c76e12ca61948fc7395ed30e
rojojun <zoloman316@gmail.com>
2023-12-11 21:48:39 +0900

    수정 사항 발생

--- task1/pages/QuizGPT.py
+++ task1/pages/QuizGPT.py
@@ -1,3 +1,51 @@
 import streamlit as st
+from langchain.retrievers import WikipediaRetriever
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+
+st.set_page_config(
+    page_title="QuizGPT",
+    page_icon="🙋"
+)
 
 st.title("QuizGPT")
+
+
+@st.cache_data(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+
+with st.sidebar:
+    choice = st.selectbox(
+        "선택",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
+    if choice == "File":
+        file = st.file_uploader(
+            ".docx , .txt , .pdf file",
+            type=["pdf", "txt", "docx"],
+        )
+        if file:
+            docs = split_file(file)
+            st.write(docs)
+    else:
+        topic = st.text_input("위키 검색 중...")
+        if topic:
+            retriever = WikipediaRetriever(top_k_results=5)
+            with st.status("위키 검색 중..."):
+                docs = retriever.get_relevant_documents(topic)
